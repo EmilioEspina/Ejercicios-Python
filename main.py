c383ec01bef5eb3e9185f61ca53b58057f9c05d9
Emilio Stefann Espina Bello <emilio.espina@inacapmail.cl>
2026-04-15 11:13:27 -0400

    limpiesa
    se saco la kk

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 tengo_swag = True
-print("caca")
+
 
 entrada = input("Pantalon chupin si o no: ")
 
 entrada_minus = entrada.lower
 if entrada_minus == "si":
     tengo_swag = False
-    
+
 if tengo_swag == True:
     print("swagger")
