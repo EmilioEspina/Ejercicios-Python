825ac8a1224acf0ded68eb888ad64b84bdc18666
Emilio Stefann Espina Bello <emilio.espina@inacapmail.cl>
2026-04-15 11:12:14 -0400

    check
    se añadio check del imput

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ print("caca")
 entrada = input("Pantalon chupin si o no: ")
 
 entrada_minus = entrada.lower
-
+if entrada_minus == "si":
+    tengo_swag = False
+    
 if tengo_swag == True:
     print("swagger")
